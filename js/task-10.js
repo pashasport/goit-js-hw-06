a53df383aaf742d911e14c26a2c759f81b34043a
pashasport <123305807+pashasport@users.noreply.github.com>
2023-06-11 13:19:59 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,13 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-const numberEl = document.querySelector("#controls");
-const buttonCreateEl = document.querySelector("[data-create]");
-const buttonDestroyEl = document.querySelector("[data-destroy]");
-const createBoxesEl = document.querySelector('#boxes')
-console.log(buttonCreateEl);
-buttonCreateEl.
-let amount = 0;
+
+const amountEl = document.querySelector("#controls input");
+const createEl = document.querySelector("button[data-create]");
+const destroyEl = document.querySelector("button[data-destroy]");
+const boxesEl = document.querySelector("#boxes");
+
 function createBoxes(amount) {
-  
+  for (var i = 0; i < amount; i += 1) {
+    var elem = document.createElement("div");
+
+    elem.style.width = 30 + 10 * i + "px";
+    elem.style.height = 30 + 10 * i + "px";
+    elem.style.backgroundColor = getRandomHexColor();
+
+    boxesEl.append(elem);
+  }
 }
+
+createEl.addEventListener("click", () => {
+  var amount = amountEl.value;
+  if (amount > 0) {
+    createBoxes(amount);
+  }
+});
+
+destroyEl.addEventListener("click", () => {
+  boxesEl.innerHTML = "";
+});
